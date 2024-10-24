c95500fcdcae16fe422436bf57dd9f8f24a3855b
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-10-24 18:52:15 +0300

    added url constants, getIngredients

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,36 @@
 <template>
-  <AppLayout imgUrl="/src/assets/img/bg-1.jpg" />
+  <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+    <div class="wrapper">
+      <div class="info">
+        <div class="title">Choose your drink</div>
+        <div class="line"></div>
+      </div>
+    </div>
+  </AppLayout>
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
 import AppLayout from "../components/AppLayout.vue";
+import { useRootStore } from "@/stores/root";
+
+const rootStore = useRootStore();
+rootStore.getIngredients();
+
+const { ingridients } = storeToRefs(rootStore);
 </script>
 
 <style lang="scss" scoped>
 @use "../assets/styles/main" as *;
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .info {
+    padding: 80px 0;
+    text-align: center;
+  }
+}
 </style>
