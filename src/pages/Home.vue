e4f0b1d8d854ca9a400db9be4f08cf9f269e26b7
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2024-10-25 00:38:28 +0300

    getCocktails

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,12 +4,38 @@
       <div class="info">
         <div class="title">Choose your drink</div>
         <div class="line"></div>
+        <div class="select-wrapper">
+          <el-select
+            v-model="ingridient"
+            placeholder="Choose main ingredient"
+            class="select"
+            size="large"
+            @change="getCocktails"
+          >
+            <el-option
+              v-for="item in ingridients"
+              :key="item.strIngredient1"
+              :label="item.strIngredient1"
+              :value="item.strIngredient1"
+            />
+          </el-select>
+        </div>
+        <div class="text">
+          Try our delicious cocktail recipes for every occasion. Find delicious
+          cocktail recipes by ingredient through our cocktail generator.
+        </div>
+        <img
+          src="../assets/img/cocktails.png"
+          alt="cocktails"
+          class="cocktails"
+        />
       </div>
     </div>
   </AppLayout>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import AppLayout from "../components/AppLayout.vue";
 import { useRootStore } from "@/stores/root";
@@ -17,7 +43,13 @@ import { useRootStore } from "@/stores/root";
 const rootStore = useRootStore();
 rootStore.getIngredients();
 
-const { ingridients } = storeToRefs(rootStore);
+const { ingridients, cocktails } = storeToRefs(rootStore);
+
+const ingridient = ref(null);
+
+function getCocktails() {
+  rootStore.getCocktails(ingridient.value);
+}
 </script>
 
 <style lang="scss" scoped>
@@ -31,6 +63,28 @@ const { ingridients } = storeToRefs(rootStore);
   .info {
     padding: 80px 0;
     text-align: center;
+
+    .select-wrapper {
+      padding-top: 50px;
+
+      .select {
+        width: 220px;
+        background-color: cadetblue;
+      }
+    }
+
+    .text {
+      max-width: 516px;
+      margin: 0 auto;
+      padding-top: 50px;
+      color: $textMuted;
+      line-height: 36px;
+      letter-spacing: 0.1em;
+    }
+
+    .cocktails {
+      margin-top: 60px;
+    }
   }
 }
 </style>
